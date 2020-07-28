fe917079463ed91ae031012f5083642a4854d709
Jeff Rock <SunShineOneX@gmail.com>
2020-07-28 19:38:23 -0400

    creation of navbar components

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import react from "react";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import axios from "axios";
+
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1> hello Navbar!!</h1>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
