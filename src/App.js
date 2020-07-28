123a27d2d1c4395a68e6a5e7e8f24fbf4d51c897
Jeff Rock <SunShineOneX@gmail.com>
2020-07-28 19:38:49 -0400

    navbar import working properly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
 import axios from "axios";
 import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom";
 
