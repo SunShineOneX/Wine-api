22cae1d919cd32c9578985eb3bbd390c5321280b
Jeff Rock <SunShineOneX@gmail.com>
2020-07-28 20:45:12 -0400

    wine01 updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,76 +1,101 @@
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import axios from "axios";
-
 import React, { Component } from "react";
+import Wine01 from "./Wine01";
+import Wine02 from "./Wine01";
+
 
 class Navbar extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //       info: [],
+    //       id: [],
+    //     };
+    //   }
+    
+    //   async getWineInfo() {
+    //     try {
+    //       const res = await axios.get(api_call);
+    //       this.setState({ info: res.data });
+    //       this.setState({ id: this.state.info[0]});
+          
+    //     } catch {
+    //       console.log("error!!!!");
+    //     }
+    //   }
+    //   componentDidMount() {
+    //     this.getWineInfo();
+    //   }
+    
   render() {
     return (
-        <Router>
-      <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a class="navbar-brand" href="#">
-            Wine Finder
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
+      <Router>
+        <div>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">
+              Wine Finder
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">
-                  Action
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Dropdown
                 </a>
-                <a class="dropdown-item" href="#">
-                  Another action
-                </a>
-                <a class="dropdown-item" href="#">
-                  Something else here
-                </a>
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-                <a class="dropdown-item" href="#">
-                  Action
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#"><Link to ="/Today">
+                    Wine01
+                    </Link>
+                  </a>
                   <a class="dropdown-item" href="#">
-                  Action
-                </a>
-                </a>
-              </div>
-            </ul>
-          </div>
-        </nav>
-        <Switch>
-            {/* <Route exact path="/wines" component={} />
-            <Route path ="/wines/merlot" component={}/> */}
-        </Switch>
-      </div>
+                    Another action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Action
+                    <a class="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </a>
+                </div>
+              </ul>
+            </div>
+          </nav>
+          <Switch>
+            <Route exact path="/Today" component={Wine01} />
+            <Route path ="/wines/merlot" component={Wine02}/>
+          </Switch>
+        </div>
       </Router>
     );
   }
--- src/components/Wine01.js
+++ src/components/Wine01.js
@@ -1,27 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import axios from "axios";
 const api_call = "http://myapi-profstream.herokuapp.com/api/ff9219/wines";
 
- class Wine01 extends Component {
-     constructor(props) {
-         super(props)
-         this.state = {
-             info: [],
-         }
-     }
+class Wine01 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      baseInfo: [],
+      id: [],
+    };
+  }
 
-     async getWineInfo() {
-         try {
-             const res = await axios.get(api_call)
-         }
-     }
-     
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+  async getWineInfo() {
+    try {
+      const res = await axios.get(api_call);
+      this.setState({ baseInfo: res.data[0] });
+      this.setState({ id: this.state.baseInfo.id});
+      
+    } catch {
+      console.log("error!!!!");
+    }
+  }
+  componentDidMount() {
+    this.getWineInfo();
+  }
+
+  render() {
+    return (
+    
+        <div>
+            <h1>Hello world!!!!!!</h1>
+            <h1>{this.state.baseInfo.id}</h1>
+            <h2>{this.state.baseInfo.name}</h2>
+            <h2>{this.state.baseInfo.year}</h2>
+        </div>
+    )
     }
 }
 
